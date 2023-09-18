ad3a66bd0c0a62fb2dd9ba909845794c2fad9eb2
aryafirmansyah2 <aryafirmansyah200404@example.com>
2023-09-18 19:49:19 +0700

    created function chart

---
+++ resources/js/mychart.js
@@ -0,0 +1,29 @@
+import Chart from "chart.js/auto";
+
+const labels = ["January", "February", "March", "April", "May", "June"];
+
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: "My First dataset",
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            data: [0, 10, 5, 2, 20, 45, 50],
+        },
+    ],
+};
+
+const config = {
+    type: "line",
+    data: data,
+    options: {
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    },
+};
+
+new Chart(document.getElementById("myChart"), config);
