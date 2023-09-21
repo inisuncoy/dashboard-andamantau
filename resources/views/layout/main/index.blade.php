405a85cdc7adc9c58788fb72a581c105f5359b43
aryafirmansyah2 <aryafirmansyah200404@example.com>
2023-09-21 20:14:28 +0700

    meanambah posision relative di parent

--- resources/views/layout/main/index.blade.php
+++ resources/views/layout/main/index.blade.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Dashboard</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
-    <div class="p-5">
-        @include('components.sidebar')
+  <div class="p-5 relative ">
+    @include('components.sidebar')
 
-        <div class="ml-[264px] px-5">
-            @yield('pages')
-        </div>
-
+    <div class="ml-[264px] px-5">
+      @yield('pages')
     </div>
+
+  </div>
 </body>
+
 </html>
