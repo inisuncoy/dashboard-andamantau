0a481ab636ec43e828a7f8c74cde0be90e701394
aryafirmansyah2 <aryafirmansyah200404@example.com>
2023-09-21 20:17:15 +0700

    update route dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboarController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.dashboard.index');
-});
+Route::get('/', [DashboarController::class,'index']);
 
 Route::get('/laporan', function () {
     return view('pages.laporan.index');
