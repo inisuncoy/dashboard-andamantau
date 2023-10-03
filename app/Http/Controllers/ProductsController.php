b28baa1e8e8514dd663c9fc43ecb8409950dd417
aryafirmansyah2 <aryafirmansyah200404@example.com>
2023-10-03 13:22:58 +0700

    update controller store

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Alert;
+use Illuminate\Support\Facades\Storage;
 
 class ProductsController extends Controller
 {
@@ -19,8 +20,11 @@ class ProductsController extends Controller
         }
 
         $token = session('token');
+        $user_id = session('userData')['id'];
 
-        $apiResponse = Http::withToken($token)->get(env('BACKEND_URL') . '/api/dashboard/umkm/products');
+        $apiResponse = Http::withToken($token)->post(env('BACKEND_URL') . '/api/products', [
+            'id_user' => $user_id,
+        ]);
 
         if ($apiResponse->failed()) {
             $errors = $apiResponse->json();
@@ -45,15 +49,17 @@ class ProductsController extends Controller
 
         $apiResponse = Http::withToken($token)->get(env('BACKEND_URL') . '/api/category/products');
 
-        if ($apiResponse->failed()) {
-            $errors = $apiResponse->json();
-            return back()->withErrors($errors)->withInput();
-        }
+        // dd($apiResponse->json());
 
-        $categories = $apiResponse->json()['data'];
+        // if ($apiResponse->failed()) {
+        //     $errors = $apiResponse->json();
+        //     return back()->withErrors($errors)->withInput();
+        // }
+
+        // $categories = $apiResponse->json()['data'];
 
         return view('pages.produk.tambah.index', [
-            'categories' => $categories,
+            // 'categories' => $categories,
         ]);
     }
 
@@ -63,27 +69,94 @@ class ProductsController extends Controller
     public function store(Request $request)
     {
         $token = session('token');
+        $fileData = [];
 
-        $apiResponse = Http::withToken($token)->post(env('BACKEND_URL') . "/api/product", [
-            'id_category_product' => $request->id_category_product,
-            'name' => $request->name,
-            'description' => $request->description,
-            'cost' => $request->cost,
-            'price' => $request->price,
-            'stock' => $request->stock,
-            'status' => $request->status,
-            'variant' => $request->variant,
-            'weight' => $request->weight,
-            'length' => $request->length,
-            'file' => $request->file('file')
-        ]);
-
-        if ($apiResponse->failed()) {
-            $errors = $apiResponse->json();
-            dd($errors);
+        // Cek dan mengunggah file1
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            $imageName = str_replace(' ', '', $file->getClientOriginalName());
+            $file->storeAs('temp', $imageName, 'public');
+            $filePath = storage_path('app/public/temp/' . $imageName);
+            $fileData[] = [
+                'name' => 'file1',
+                'contents' => file_get_contents($filePath),
+                'filename' => $imageName,
+            ];
         }
 
-        dd($apiResponse->json());
+        // Cek dan mengunggah file2, file3, file4, dan file5
+        for ($i = 2; $i <= 5; $i++) {
+            $fileInputName = 'file' . $i;
+            if ($request->hasFile($fileInputName)) {
+                $file = $request->file($fileInputName);
+                $imageName = str_replace(' ', '', $file->getClientOriginalName());
+                $file->storeAs('temp', $imageName, 'public');
+                $filePath = storage_path('app/public/temp/' . $imageName);
+                $fileData[] = [
+                    'name' => $fileInputName,
+                    'contents' => file_get_contents($filePath),
+                    'filename' => $imageName,
+                ];
+            }
+        }
+
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            $imageName = str_replace(' ', '', $file->getClientOriginalName());
+            $file->storeAs('temp', $imageName, 'public');
+            $filePath = storage_path('app/public/temp/' . $imageName);
+
+            if (!empty($fileData)) {
+                $response = Http::withToken($token);
+                
+                foreach ($fileData as $fileItem) {
+                    $response->attach(
+                        'file',
+                        file_get_contents($filePath),
+                        $imageName
+                    )->attach(
+                        $fileItem['name'], 
+                        $fileItem['contents'], 
+                        $fileItem['filename']
+                    );
+                }
+        
+                $apiResponse  = $response->post(env('BACKEND_URL') . "/api/dashboard/umkm/product", [
+                    'name' => $request->name,
+                    'id_category_product' => $request->id_category_product,
+                    'status' => $request->status,
+                    'price' => $request->price,
+                    'description' => $request->description,
+                    'stock' => $request->stock,
+                    'variant' => $request->variant,
+                    'weight' => $request->weight,
+                    'width' => $request->width,
+                    'length' => $request->length,
+                    'height' => $request->height,
+                ]);
+        
+                dd($apiResponse->json());
+            } else {
+                $apiResponse = Http::attach(
+                    'file',
+                    file_get_contents($filePath),
+                    $imageName
+                )->withToken($token)->post(env('BACKEND_URL'). "/api/dashboard/umkm/product", [
+                    'name' => $request->name,
+                    'id_category_product' => $request->id_category_product,
+                    'status' => $request->status,
+                    'price' => $request->price,
+                    'description' => $request->description,
+                    'stock' => $request->stock,
+                    'variant' => $request->variant,
+                    'weight' => $request->weight,
+                    'width' => $request->width,
+                    'length' => $request->length,
+                    'height' => $request->height,
+                ]);
+                dd($apiResponse->json());
+            }
+        } 
     }
 
     /**
